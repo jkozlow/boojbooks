e62e18d8bdd589216d66bf0630a3e85570a78678
Josh Kozlowski <jkozlow@outlook.com>
2017-02-03 08:28:42 -0700

    migrations work

--- database/migrations/2017_02_03_144642_books.php
+++ database/migrations/2017_02_03_144642_books.php
@@ -13,7 +13,20 @@ class Books extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('external_id');
+            $table->integer('booklist_id');
+            $table->string('name', 1000);
+            $table->string('author');
+            $table->text('description');
+            $table->text('notes');
+            $table->string('pagenum', 100);
+            $table->string('page_count', 100);
+            $table->string('infourl', 1000);
+            $table->string('imageurl', 1000);
+            $table->string('category', 1000);
+        });
     }
 
     /**
